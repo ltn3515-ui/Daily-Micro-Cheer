1c7c8b0a81e92f9ab349c6e839834e6cb1f3ed4b
ltn3515-ui <ltn3515@gmail.com>
2026-09-17 10:18:58 +0900

    feat: add Apps in Toss config

---
+++ apps-in-toss.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from '@apps-in-toss/web-framework/config';
+
+export default defineConfig({
+  appName: 'daily-micro-cheer',
+  brand: { primaryColor: '#FF8A65' },
+  webView: {},
+  permissions: [],
+  webBundleDir: 'dist',
+});
